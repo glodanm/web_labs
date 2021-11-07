0158ebaade2581d674ea9af72368df6ed7794560
glodanm <glodan196915@gmail.com>
2021-11-07 23:49:39 +0200

    Added React.js home page for Lab 6

---
+++ lab-6/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from "./Components/Header/header";
+
+
+function App() {
+  return (
+    <div className="App">
+    <Header />
+    {/* <Body />
+    <Footer /> */}
+    </div>
+  );
+}
+
+export default App;
